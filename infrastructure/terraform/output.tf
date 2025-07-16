e60e6d55bddc2553842f55e7382504de965dd6be
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2025-07-16 18:40:29 +0200

    Update output.tf

--- infrastructure/terraform/output.tf
+++ infrastructure/terraform/output.tf
@@ -2,11 +2,10 @@ output "bastion_ip" {
   value = aws_instance.bastion.public_ip
 }
 
-output "master_ip" {
-  value = aws_instance.master.public_ip
+output "master_private_ip" {
+  value = aws_instance.master.private_ip
 }
 
-output "worker_ip" {
-  value = aws_instance.worker.public_ip
+output "worker_private_ip" {
+  value = aws_instance.worker.private_ip
 }
-
