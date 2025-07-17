6020a90bad61efcf819d5e51322d3a8253fa3a8a
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2025-07-17 22:18:59 +0200

    Update generate_inventory.sh

--- infrastructure/ansible/generate_inventory.sh
+++ infrastructure/ansible/generate_inventory.sh
@@ -15,19 +15,20 @@ INVENTORY_DIR="${SCRIPT_DIR}/inventory"
 # Create inventory directory if not exists
 mkdir -p "$INVENTORY_DIR"
 
-# Write inventory file
-cat <<EOF > "${INVENTORY_DIR}/hosts.ini"
-[bastion]
-${BASTION_IP}
+# Define private key path used inside Jenkins container
+KEY_PATH=~/.ssh/jenkins-key.pem
 
+# Write inventory file with jump host config
+cat <<EOF > "${INVENTORY_DIR}/hosts.ini"
 [master]
-${MASTER_IP}
+${MASTER_IP} ansible_user=ec2-user ansible_ssh_private_key_file=$KEY_PATH ansible_ssh_common_args='-o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ec2-user@$BASTION_IP"'
 
 [worker]
-${WORKER_IP}
+${WORKER_IP} ansible_user=ec2-user ansible_ssh_private_key_file=$KEY_PATH ansible_ssh_common_args='-o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ec2-user@$BASTION_IP"'
+
+[bastion]
+${BASTION_IP} ansible_user=ec2-user ansible_ssh_private_key_file=$KEY_PATH
 
 [all:vars]
-ansible_user=ec2-user
 ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOF
-
